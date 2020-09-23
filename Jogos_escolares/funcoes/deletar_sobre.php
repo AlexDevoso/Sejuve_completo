21f2a9aa55e528fc43b96244f7ffa3535b389980
AlexLlima <36746185+AlexLlima@users.noreply.github.com>
2020-09-23 10:33:41 -0300

    Add files via upload

--- Jogos_escolares/funcoes/deletar_sobre.php
+++ Jogos_escolares/funcoes/deletar_sobre.php
@@ -1,13 +1,20 @@
 <?php
     session_start();
     include('conexao.php');
+
     if(isset($_POST['deletar_funcionario'])){
+
         $id_fun = mysqli_escape_string($conexao, $_POST['id_fun']);
+
         $mostrar_img = "SELECT imagem_fun from funcionario where id_fun = '$id_fun'";
-        $result_img = mysqli_query($mostrar_img);
+        $result_img = mysqli_query($conexao, $mostrar_img);
         $dados_img = mysqli_fetch_array($result_img);
-        $sql = "DELETE from funcionario where id_fun = '$id_fun'";
-        if(mysqli_query($conexao, $sql)){
+
+        $sql_delete_freq = "DELETE from frequencia_fun where fk_id = '$id_fun'";
+        $sql_delete = "DELETE FROM funcionario where id_fun = '$id_fun'";
+
+        if(mysqli_query($conexao, $sql_delete_freq)){
+        if(mysqli_query($conexao, $sql_delete)){
             unlink("../../imagem/".$dados_img['imagem_fun']);
             header('Location: ../interface/editar_pagina.php');
         }
@@ -15,7 +22,12 @@
             header('Location: ../interface/editar_pagina.php');
         }
     }
-    elseif(isset($_POST['deletar_sele'])){
+    else{
+        header('Location: ../interface/editar_pagina.php');
+    }
+    }
+
+    else if(isset($_POST['deletar_sele'])){
         $id_sm = mysqli_escape_string($conexao, $_POST['id_sm']);
         $sql_imagens = "SELECT imagem1_sm, imagem2_sm, imagem3_sm, imagem4_sm from selecoes_municipais
         where id_sm = '$id_sm'";
@@ -44,4 +56,6 @@
             header('Location: ../interface/editar_pagina.php');
         }
     }
+
+
 ?>
