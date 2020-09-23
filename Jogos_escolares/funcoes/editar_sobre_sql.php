52f4fe8175ffad4143057b8fb4b2c598536bcc66
AlexLlima <36746185+AlexLlima@users.noreply.github.com>
2020-09-23 09:40:23 -0300

    Add files via upload

--- Jogos_escolares/funcoes/editar_sobre_sql.php
+++ Jogos_escolares/funcoes/editar_sobre_sql.php
@@ -82,4 +82,276 @@
 
 
     }
+
+    else{
+
+        if(isset($_POST['editar_sobre_sele1'])){
+
+        
+        $id_sm = mysqli_escape_string($conexao, $_POST['id_sm']);
+
+        $titul_sm1 = mysqli_escape_string($conexao, $_POST['titul_sm1']);
+
+        $desc1_sm = mysqli_escape_string($conexao, $_POST['desc1_sm']);
+
+        $imagem1_sm = $_FILES['imagem1_sm'];
+        
+
+
+         //se a logo foi selecionada
+    if (!empty($imagem1_sm["name"])) {
+
+     
+        
+        //máxima em piexels
+        $lagura = 7000;
+        $altura = 7000;
+        //máximo em bytes
+        $tamanho = 500000;
+        $error = array();
+        //verifica se o arquivo é uma imagem
+        if (!preg_match("/^image\/(pjpeg|jpeg|png|gif|bmp)$/", $imagem1_sm['type'])) {
+            $error[1] = "ISSO NÃO É uma imagem";
+            header('Location: ../interface/editar_pagina1.php');
+        }
+        //pega as dimensões da imagem
+        $dimensões = getimagesize($imagem1_sm["tmp_name"]);
+
+        //verifica se a largura da imagem é maior que largura permitida
+        if ($dimensões[0] > $lagura) {
+            $error[2] = "A largura da imagem não deve ultrapassar " . $lagura . "pixels";
+            header('Location: ../interface/editar_pagina2.php');
+        }
+        //verifica se a altura da imagem é maior que a altura permitida
+        if ($dimensões[0] > $altura) {
+            $error[3] = "A altura da imagem não deve ultrapassar " . $altura . " pixels";
+            header('Location: ../interface/editar_pagina3.php');
+        }
+        //verifica o tamanho em bytes da imagem
+        if ($imagem1_sm["size"] > $tamanho) {
+            $error[4] = "A imagem deve ter no máximo " . $tamanho . " bytes";
+            header('Location: ../interface/editar_pagina4.php');
+        }
+        //se não houver erro
+        if (count($error) == 0) {
+            //pega a extensão da imagem
+            preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $imagem1_sm["name"], $ext);
+            //gera um nome único para a imagem
+            $nome_imagem = md5(uniqid()) . "." . $ext[1];
+            //camihno de onde ficará a imagem
+            $caminho_imagem = "../../imagem/" . $nome_imagem;
+            //faz o upload da imagem para seu rad2deg(number)espectivo caminho
+            move_uploaded_file($imagem1_sm["tmp_name"], $caminho_imagem);
+
+
+            //inserindo dados no banco1
+            $sql = "UPDATE selecoes_municipais set titul_sm1 = '$titul_sm1', desc1_sm = '$desc1_sm', 
+            imagem1_sm = '$nome_imagem' where id_sm = '$id_sm'";
+            if (mysqli_query($conexao, $sql)) {
+                header('Location: ../interface/editar_pagina.php');
+                $_SESSION['login'][1] = "ATUALIZADO COM SUCESSO";
+            }
+            else{
+                header('Location: ../interface/editar_pagina2.php');
+                
+            }
+        }
+
+
+    }
+    else{
+           
+        $sql = "UPDATE selecoes_municipais set titul_sm1 = '$titul_sm1', desc1_sm = '$desc1_sm'
+         where id_sm = '$id_sm'";
+        
+        if (mysqli_query($conexao, $sql)) {
+            header('Location: ../interface/editar_pagina.php');
+            $_SESSION['login'][1] = "ATUALIZADO COM SUCESSO";
+        }
+        else{
+             header('Location: ../interface/editar_pagina.php');
+            $_SESSION['login'][2] = " ERRO AO ATUALIZADO COM SUCESSO";
+        }
+    }
+    }
+
+    else if(isset($_POST['editar_sobre_sele2'])){
+
+        
+        $id_sm = mysqli_escape_string($conexao, $_POST['id_sm']);
+    
+        $titul_sm2 = mysqli_escape_string($conexao, $_POST['titul_sm2']);
+     
+        $desc2_sm = mysqli_escape_string($conexao, $_POST['desc2_sm']);
+
+        $imagem2_sm = $_FILES['imagem2_sm'];
+
+
+         //se a logo foi selecionada
+    if (!empty($imagem2_sm["name"])) {
+
+     
+        
+        //máxima em piexels
+        $lagura = 7000;
+        $altura = 7000;
+        //máximo em bytes
+        $tamanho = 500000;
+        $error = array();
+        //verifica se o arquivo é uma imagem
+        if (!preg_match("/^image\/(pjpeg|jpeg|png|gif|bmp)$/", $imagem2_sm['type'])) {
+            $error[1] = "ISSO NÃO É uma imagem";
+            header('Location: ../interface/editar_pagina1.php');
+        }
+        //pega as dimensões da imagem
+        $dimensões = getimagesize($imagem2_sm["tmp_name"]);
+
+        //verifica se a largura da imagem é maior que largura permitida
+        if ($dimensões[0] > $lagura) {
+            $error[2] = "A largura da imagem não deve ultrapassar " . $lagura . "pixels";
+            header('Location: ../interface/editar_pagina.php');
+        }
+        //verifica se a altura da imagem é maior que a altura permitida
+        if ($dimensões[0] > $altura) {
+            $error[3] = "A altura da imagem não deve ultrapassar " . $altura . " pixels";
+            header('Location: ../interface/editar_pagina.php');
+        }
+        //verifica o tamanho em bytes da imagem
+        if ($imagem2_sm["size"] > $tamanho) {
+            $error[4] = "A imagem deve ter no máximo " . $tamanho . " bytes";
+            header('Location: ../interface/editar_pagina.php');
+        }
+        //se não houver erro
+        if (count($error) == 0) {
+            //pega a extensão da imagem
+            preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $imagem2_sm["name"], $ext);
+            //gera um nome único para a imagem
+            $nome_imagem = md5(uniqid()) . "." . $ext[1];
+            //camihno de onde ficará a imagem
+            $caminho_imagem = "../../imagem/" . $nome_imagem;
+            //faz o upload da imagem para seu rad2deg(number)espectivo caminho
+            move_uploaded_file($imagem2_sm["tmp_name"], $caminho_imagem);
+
+
+            //inserindo dados no banco1
+            $sql = "UPDATE selecoes_municipais set titul_sm2 = '$titul_sm2', desc2_sm = '$desc2_sm', 
+            imagem2_sm = '$nome_imagem' where id_sm = '$id_sm'";
+            if (mysqli_query($conexao, $sql)) {
+                header('Location: ../interface/editar_pagina.php');
+                $_SESSION['login'][1] = "ATUALIZADO COM SUCESSO";
+            }
+            else{
+                header('Location: ../interface/editar_pagina.php');
+                $_SESSION['login'][1] = "ERRO AO ATUALIZAR";
+            }
+        }
+
+
+    }
+    else{
+           
+        $sql = "UPDATE selecoes_municipais set titul_sm2 = '$titul_sm2', desc2_sm = '$desc2_sm'
+         where id_sm = '$id_sm'";
+        
+        if (mysqli_query($conexao, $sql)) {
+            header('Location: ../interface/editar_pagina.php');
+            $_SESSION['login'][1] = "ATUALIZADO COM SUCESSO";
+        }
+        else{
+             header('Location: ../interface/editar_pagina.php');
+            $_SESSION['login'][2] = " ERRO AO ATUALIZADO COM SUCESSO";
+        }
+    }
+    }
+
+    elseif(isset($_POST['editar_sobre_sele3'])){
+
+        
+        $id_sm = mysqli_escape_string($conexao, $_POST['id_sm']);
+
+        $titul_sm3 = mysqli_escape_string($conexao, $_POST['titul_sm3']);
+   
+        $desc3_sm = mysqli_escape_string($conexao, $_POST['desc3_sm']);
+  
+        $imagem3_sm = $_FILES['imagem3_sm'];
+
+
+         //se a logo foi selecionada
+    if (!empty($imagem3_sm["name"])) {
+
+     
+        
+        //máxima em piexels
+        $lagura = 7000;
+        $altura = 7000;
+        //máximo em bytes
+        $tamanho = 500000;
+        $error = array();
+        //verifica se o arquivo é uma imagem
+        if (!preg_match("/^image\/(pjpeg|jpeg|png|gif|bmp)$/", $imagem3_sm['type'])) {
+            $error[1] = "ISSO NÃO É uma imagem";
+            header('Location: ../interface/editar_pagina1.php');
+        }
+        //pega as dimensões da imagem
+        $dimensões = getimagesize($imagem3_sm["tmp_name"]);
+
+        //verifica se a largura da imagem é maior que largura permitida
+        if ($dimensões[0] > $lagura) {
+            $error[2] = "A largura da imagem não deve ultrapassar " . $lagura . "pixels";
+            header('Location: ../interface/editar_pagina2.php');
+        }
+        //verifica se a altura da imagem é maior que a altura permitida
+        if ($dimensões[0] > $altura) {
+            $error[3] = "A altura da imagem não deve ultrapassar " . $altura . " pixels";
+            header('Location: ../interface/editar_pagina3.php');
+        }
+        //verifica o tamanho em bytes da imagem
+        if ($imagem3_sm["size"] > $tamanho) {
+            $error[4] = "A imagem deve ter no máximo " . $tamanho . " bytes";
+            header('Location: ../interface/editar_pagina4.php');
+        }
+        //se não houver erro
+        if (count($error) == 0) {
+            //pega a extensão da imagem
+            preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $imagem3_sm["name"], $ext);
+            //gera um nome único para a imagem
+            $nome_imagem = md5(uniqid()) . "." . $ext[1];
+            //camihno de onde ficará a imagem
+            $caminho_imagem = "../../imagem/" . $nome_imagem;
+            //faz o upload da imagem para seu rad2deg(number)espectivo caminho
+            move_uploaded_file($imagem3_sm["tmp_name"], $caminho_imagem);
+
+
+            //inserindo dados no banco1
+            $sql = "UPDATE selecoes_municipais set titul_sm3 = '$titul_sm3', desc3_sm = '$desc3_sm', 
+            imagem3_sm = '$nome_imagem' where id_sm = '$id_sm'";
+            if (mysqli_query($conexao, $sql)) {
+                header('Location: ../interface/editar_pagina.php');
+                $_SESSION['login'][1] = "ATUALIZADO COM SUCESSO";
+            }
+            else{
+                header('Location: ../interface/editar_pagina2.php');
+                
+            }
+        }
+
+
+    }
+    else{
+           
+        $sql = "UPDATE selecoes_municipais set titul_sm3 = '$titul_sm3', desc3_sm = '$desc3_sm'
+         where id_sm = '$id_sm'";    
+        
+        if (mysqli_query($conexao, $sql)) {
+            header('Location: ../interface/editar_pagina.php');
+            $_SESSION['login'][1] = "ATUALIZADO COM SUCESSO";
+        }
+        else{
+             header('Location: ../interface/editar_pagina.php');
+            $_SESSION['login'][2] = " ERRO AO ATUALIZADO COM SUCESSO";
+        }
+    }
+    }
+
+}
 ?>
