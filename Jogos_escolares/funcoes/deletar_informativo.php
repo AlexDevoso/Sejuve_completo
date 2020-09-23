ed56db598d93fbe8cbc96361f40435fafebecc03
AlexLlima <36746185+AlexLlima@users.noreply.github.com>
2020-09-23 11:09:55 -0300

    Add files via upload

--- Jogos_escolares/funcoes/deletar_informativo.php
+++ Jogos_escolares/funcoes/deletar_informativo.php
@@ -35,21 +35,32 @@ include('conexao.php');
     }
 
     elseif(isset($_POST['deletar_noticia_total'])){
+
         $id_no = mysqli_escape_string($conexao, $_POST['id_no']);
         
         $mostrar_imagem_nt = "SELECT imagem_no from noticia where id_no = '$id_no'";
         $result_mostrar_nt = mysqli_query($conexao, $mostrar_imagem_nt);
         $dados_mostrar_nt = mysqli_fetch_array($result_mostrar_nt);
         
+        $sql_np = "DELETE from noticia_painel where fk_noticia = '$id_no'";
         $sql_nt = "DELETE from noticia where id_no = '$id_no'";
         
-        if(mysqli_query($conexao, $sql_nt)){
-            unlink("../../imagem/".$$dados_mostrar_nt['imagem_no']);
-            header('Location: ../interface/editar_pagina.php');
+        if(mysqli_query($conexao, $sql_np)){
+
+            if(mysqli_query($conexao, $sql_nt)){
+                unlink("../../imagem/".$$dados_mostrar_nt['imagem_no']);
+                header('Location: ../interface/editar_pagina.php');
+            }
+            else{
+                header('Location: ../interface/editar_pagina.php');
+            }
+
+        
         }
         else{
-            header('Location: editar_pagina.php');
+            header('Location ../inferface/editar_pagina.php');
         }
+
     }
 
 
